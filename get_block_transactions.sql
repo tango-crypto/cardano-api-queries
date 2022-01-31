a492273cd5ae80289e1088448f75a7e6f27b4a5b
Leobel Izquierdo <leobelizquierdo@gmail.com>
2022-01-31 16:35:24 -0300

    Create get_block_transactions.sql

---
+++ get_block_transactions.sql
@@ -0,0 +1,17 @@
+SELECT "tx"."id",
+	encode(tx.hash,'hex') as hash,
+	"tx"."block_id",
+	"tx"."block_index",
+	"tx"."out_sum",
+	"tx"."fee",
+	"tx"."deposit",
+	"tx"."size",
+	"tx"."invalid_before",
+	"tx"."invalid_hereafter",
+	"tx"."valid_contract",
+	"tx"."script_size"
+FROM "tx"
+INNER JOIN "block" ON "tx"."block_id" = "block"."id"
+WHERE BLOCK.BLOCK_NO = 3174184 and tx.id < 260018
+ORDER BY "tx"."id" DESC
+LIMIT 50
