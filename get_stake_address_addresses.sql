d670c88caf96fdca26da70c76c4b0903e7e60890
Leobel Izquierdo <leobelizquierdo@gmail.com>
2022-02-01 16:59:56 -0300

    Update get_stake_address_addresses.sql

--- get_stake_address_addresses.sql
+++ get_stake_address_addresses.sql
@@ -1,14 +1,9 @@
-SELECT "a"."tx_id",
-	"a"."index",
-	"a"."address"
-FROM
-	(SELECT MAX(TX_OUT.TX_ID) AS TX_ID,
-			MAX(TX_OUT.INDEX) AS INDEX,
-			"tx_out"."address"
-		FROM "tx_out"
-		INNER JOIN "stake_address" AS "sa" ON "tx_out"."stake_address_id" = "sa"."id"
-		WHERE "sa"."view" = 'stake_test1urtt0tpxwxyll6gclxnz5srjx3zjr099pgrqkd3st7339tcr0u0ph'
-		GROUP BY "tx_out"."address") AS "a"
-WHERE A.TX_ID < 58574 OR (A.TX_ID = 58574 AND A.INDEX < 0)
-ORDER BY A.TX_ID DESC, A.INDEX DESC
-LIMIT 50
+select 
+DISTINCT ON (tx_out.address) 
+tx_out.address
+FROM "tx_out"
+INNER JOIN "stake_address" AS "sa" ON "tx_out"."stake_address_id" = "sa"."id"
+WHERE "sa"."view" = 'stake_test1urtt0tpxwxyll6gclxnz5srjx3zjr099pgrqkd3st7339tcr0u0ph'
+AND tx_out.address < 'addr_test1qrfukqypvy4efts3jy4x0xvcrz406g3z00f8w570kmky2vkkk7kzvuvfll5337dx9fq8ydz9yx722zsxpvmrqharz2hscvtazf'
+order by tx_out.address DESC
+limit 50
