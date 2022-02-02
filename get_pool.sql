89b656d8510bb2cc8d3332d2677f393f2454a833
Leobel Izquierdo <leobelizquierdo@gmail.com>
2022-02-02 15:18:11 -0300

    Update get_pool.sql

--- get_pool.sql
+++ get_pool.sql
@@ -1,33 +1,17 @@
-SELECT 
-"pool_hash"."view" AS "pool_id",
-ENCODE(POOL_HASH.HASH_RAW,'hex') AS RAW_ID,
-"pmr"."url",
-ENCODE(pmr.HASH,'hex') AS HASH,
-POD.JSON AS DATA,
-pmr.registered_tx_id
-FROM "pool_hash"
-LEFT JOIN "pool_metadata_ref" AS "pmr" ON "pmr"."pool_id" = "pool_hash"."id"
-LEFT JOIN "pool_offline_data" AS "pod" ON "pod"."pmr_id" = "pmr"."id"
-WHERE "pool_hash"."view" = 'pool1cr8vpy3ta3smcxjq8hfu8n2chxhtc78ukfruqjhfgarf5azypen'
-ORDER by pmr.registered_tx_id DESC;
-
-SELECT 
-"pool_hash"."view" AS "pool_id",
-ENCODE(POOL_HASH.HASH_RAW,'hex') AS id,
-pu.pledge,
-pu.margin,
-pu.fixed_cost,
-pu.active_epoch_no,
-"pmr"."url",
-ENCODE(pmr.HASH,'hex') AS HASH,
-POD.JSON AS DATA
-FROM "pool_update" as pu
-INNER JOIN pool_hash on pool_hash.id = pu.hash_id
+SELECT "pool_hash"."view" AS "pool_id",
+	ENCODE(POOL_HASH.HASH_RAW,'hex') AS ID,
+	"pu"."pledge",
+	"pu"."margin",
+	"pu"."fixed_cost",
+	"pu"."active_epoch_no",
+	first_value ("pmr"."url") over(ORDER BY CASE WHEN pmr.url is null then 0 else "pu"."registered_tx_id" end DESC) as url,
+	first_value (encode(PMR.HASH,'hex')) over(ORDER BY CASE WHEN pmr.hash is null then 0 else "pu"."registered_tx_id" end DESC) AS HASH,
+	first_value (POD.JSON) over(ORDER BY CASE WHEN pod.json is null then 0 else "pu"."registered_tx_id" end DESC) AS DATA
+FROM "pool_update" AS "pu"
+INNER JOIN "pool_hash" ON "pool_hash"."id" = "pu"."hash_id"
+INNER JOIN "epoch" ON "epoch"."no" = "pu"."active_epoch_no"
 LEFT JOIN "pool_metadata_ref" AS "pmr" ON "pmr"."id" = "pu"."meta_id"
 LEFT JOIN "pool_offline_data" AS "pod" ON "pod"."pmr_id" = "pmr"."id"
 WHERE "pool_hash"."view" = 'pool1cr8vpy3ta3smcxjq8hfu8n2chxhtc78ukfruqjhfgarf5azypen'
-AND EXISTS (
-	SELECT 1 FROM epoch where epoch.no = pu.active_epoch_no 
-)
-ORDER by pu.registered_tx_id DESC
-LIMIT 1;
+ORDER BY "pu"."registered_tx_id" DESC
+LIMIT 1
