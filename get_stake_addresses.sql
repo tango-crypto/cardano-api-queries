7b5e47caf8ef9e1e3b2fb616f7a1c232a244f845
Leobel Izquierdo <leobelizquierdo@gmail.com>
2022-02-02 23:31:33 -0300

    Delete get_stake_addresses.sql

--- get_stake_addresses.sql
+++
@@ -1,6 +0,0 @@
-SELECT DISTINCT (TX_OUT.ADDRESS) AS ADDRESS
-FROM "tx_out"
-INNER JOIN "stake_address" AS "sa" ON "tx_out"."stake_address_id" = "sa"."id"
-WHERE SA.VIEW = 'stake_test1urtt0tpxwxyll6gclxnz5srjx3zjr099pgrqkd3st7339tcr0u0ph'
-ORDER BY "tx_out"."address" DESC
-LIMIT 50;
