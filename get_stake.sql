5d67ce3a1a3773ecc39dfec63b61ef4134d6c5a8
Leobel Izquierdo <leobelizquierdo@gmail.com>
2022-02-23 01:54:41 -0300

    Update get_stake.sql

--- get_stake.sql
+++ get_stake.sql
@@ -10,48 +10,67 @@ SELECT STAKE_ACTIVE(SD.ID,DELEGATION.TX_ID) AS ACTIVE,
 		FROM STAKE_ADDRESS AS SA
 		INNER JOIN DELEGATION DEL ON DEL.ADDR_ID = SA.ID
 		INNER JOIN POOL_HASH ON POOL_HASH.ID = DEL.POOL_HASH_ID
-		WHERE "sa"."view" = 'stake_test1uzxpncx82vfkl5ml00ws44hzfdh64r22kr93e79jqsumv0q8g8cy0'
+		WHERE "sa"."view" = 'stake1u83fqtl4xq5lxtjz47t8w0gtgzu8krrlx66prdvcxq0zl2gu0pdkz'
 		ORDER BY DEL.ACTIVE_EPOCH_NO DESC
-		LIMIT 1) AS POOL_ID
+		LIMIT 1
+    ) AS POOL_ID
 FROM "stake_address" AS "sa"
 LEFT JOIN
 	(SELECT "utxo_view"."stake_address_id",
 			SUM(UTXO_VIEW.VALUE) AS CONTROLLED_TOTAL_STAKE
 		FROM "utxo_view"
 		INNER JOIN "stake_address" AS "sa" ON "sa"."id" = "utxo_view"."stake_address_id"
-		WHERE "sa"."view" = 'stake_test1uzxpncx82vfkl5ml00ws44hzfdh64r22kr93e79jqsumv0q8g8cy0'
-		GROUP BY "utxo_view"."stake_address_id") AS "utxo_view" ON "utxo_view"."stake_address_id" = "sa"."id"
+		WHERE "sa"."view" = 'stake1u83fqtl4xq5lxtjz47t8w0gtgzu8krrlx66prdvcxq0zl2gu0pdkz'
+		GROUP BY "utxo_view"."stake_address_id"
+    ) AS "utxo_view" ON "utxo_view"."stake_address_id" = "sa"."id"
 LEFT JOIN
-	(SELECT "reward"."addr_id",
+	(
+        SELECT "reward"."addr_id",
 			SUM(REWARD.AMOUNT) AS REWARDS_SUM
 		FROM "reward"
 		INNER JOIN "stake_address" AS "sa" ON "sa"."id" = "reward"."addr_id"
-		WHERE "sa"."view" = 'stake_test1uzxpncx82vfkl5ml00ws44hzfdh64r22kr93e79jqsumv0q8g8cy0'
-		GROUP BY "reward"."addr_id") AS "reward" ON "reward"."addr_id" = "sa"."id"
+		WHERE "sa"."view" = 'stake1u83fqtl4xq5lxtjz47t8w0gtgzu8krrlx66prdvcxq0zl2gu0pdkz'
+		GROUP BY "reward"."addr_id"
+    ) AS "reward" ON "reward"."addr_id" = "sa"."id"
 LEFT JOIN
-	(SELECT "withdrawal"."addr_id",
-			SUM(WITHDRAWAL.AMOUNT) AS WITHDRAWALS_SUM
-		FROM "withdrawal"
-		INNER JOIN "stake_address" AS "sa" ON "sa"."id" = "withdrawal"."addr_id"
-		WHERE "sa"."view" = 'stake_test1uzxpncx82vfkl5ml00ws44hzfdh64r22kr93e79jqsumv0q8g8cy0'
-		GROUP BY "withdrawal"."addr_id") AS "wd" ON "wd"."addr_id" = "sa"."id"
+	(
+        SELECT w.addr_id,
+            SUM(w.AMOUNT) AS WITHDRAWALS_SUM
+        FROM (
+            SELECT DISTINCT ON (withdrawal.tx_id) "withdrawal"."addr_id", "withdrawal"."amount"
+            FROM "withdrawal"
+            INNER JOIN "stake_address" AS "sa" ON "sa"."id" = "withdrawal"."addr_id"
+            WHERE "sa"."view" = 'stake1u83fqtl4xq5lxtjz47t8w0gtgzu8krrlx66prdvcxq0zl2gu0pdkz'
+        ) w
+        GROUP BY "w"."addr_id"
+    ) AS "wd" ON "wd"."addr_id" = "sa"."id"
 LEFT JOIN
-	(SELECT "reserve"."addr_id",
-			SUM(RESERVE.AMOUNT) AS RESERVES_SUM
-		FROM "reserve"
-		INNER JOIN "stake_address" AS "sa" ON "sa"."id" = "reserve"."addr_id"
-		WHERE "sa"."view" = 'stake_test1uzxpncx82vfkl5ml00ws44hzfdh64r22kr93e79jqsumv0q8g8cy0'
-		GROUP BY "reserve"."addr_id") AS "reserve" ON "reserve"."addr_id" = "sa"."id"
+	(
+        SELECT "reserve"."addr_id",
+            SUM(reserve.AMOUNT) AS RESERVES_SUM
+        FROM (
+            SELECT DISTINCT ON ("reserve"."tx_id") "reserve"."addr_id", "reserve"."amount"
+            FROM "reserve"
+            INNER JOIN "stake_address" AS "sa" ON "sa"."id" = "reserve"."addr_id"
+            WHERE "sa"."view" = 'stake1u83fqtl4xq5lxtjz47t8w0gtgzu8krrlx66prdvcxq0zl2gu0pdkz'
+        ) AS "reserve"
+        GROUP BY "reserve"."addr_id"
+    ) AS "reserve" ON "reserve"."addr_id" = "sa"."id"
 LEFT JOIN
-	(SELECT "treasury"."addr_id",
-			SUM(TREASURY.AMOUNT) AS TREASURY_SUM
-		FROM "treasury"
-		INNER JOIN "stake_address" AS "sa" ON "sa"."id" = "treasury"."addr_id"
-		WHERE "sa"."view" = 'stake_test1uzxpncx82vfkl5ml00ws44hzfdh64r22kr93e79jqsumv0q8g8cy0'
-		GROUP BY "treasury"."addr_id") AS "treasury" ON "treasury"."addr_id" = "sa"."id"
+	(
+        SELECT "treasury"."addr_id",
+            SUM(TREASURY.AMOUNT) AS TREASURY_SUM
+        FROM (
+            SELECT DISTINCT ON ("treasury"."tx_id") "treasury"."addr_id", "treasury"."amount"
+            FROM "treasury"
+            INNER JOIN "stake_address" AS "sa" ON "sa"."id" = "treasury"."addr_id"
+            WHERE "sa"."view" = 'stake1u83fqtl4xq5lxtjz47t8w0gtgzu8krrlx66prdvcxq0zl2gu0pdkz'
+        ) AS "treasury"
+        GROUP BY "treasury"."addr_id"
+    ) AS "treasury" ON "treasury"."addr_id" = "sa"."id"
 LEFT JOIN "delegation" ON "delegation"."addr_id" = "sa"."id"
 LEFT JOIN "tx" ON "tx"."id" = "delegation"."tx_id"
 LEFT JOIN "block" ON "block"."id" = "tx"."block_id"
 LEFT JOIN "stake_deregistration" AS "sd" ON "sd"."addr_id" = "sa"."id"
-WHERE "sa"."view" = 'stake_test1uzxpncx82vfkl5ml00ws44hzfdh64r22kr93e79jqsumv0q8g8cy0'
-LIMIT 1
+WHERE "sa"."view" = 'stake1u83fqtl4xq5lxtjz47t8w0gtgzu8krrlx66prdvcxq0zl2gu0pdkz'
+LIMIT 1;
