a3be6a06611c07ac959471ad60afb2532f7140c8
Leobel Izquierdo <leobelizquierdo@gmail.com>
2023-02-18 12:39:09 -0300

    Create get_asset_utxo_metadata.sql

---
+++ get_asset_utxo_metadata.sql
@@ -0,0 +1,12 @@
+SELECT 
+    ENCODE(ASSET.POLICY,'hex') AS POLICY_ID,
+	ENCODE(ASSET.NAME,'hex') AS ASSET_NAME,
+	DATUM.VALUE AS VALUE,
+	ENCODE(DATUM.BYTES,'hex') AS METADATA
+FROM "multi_asset" AS "asset"
+INNER JOIN "ma_tx_out" AS "mto" ON "mto"."ident" = "asset"."id"
+INNER JOIN "tx_out" ON "tx_out"."id" = "mto"."tx_out_id"
+INNER JOIN "datum" ON "datum"."hash" = "tx_out"."data_hash" OR "datum"."id" = "tx_out"."inline_datum_id"
+WHERE ASSET.FINGERPRINT = 'asset17nxg4qz9wz8mwhhkryglg8myk803qfhwlv6y2r'
+ORDER BY "tx_out"."tx_id" DESC
+LIMIT 1;
