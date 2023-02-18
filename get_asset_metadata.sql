d0f11e94991a4e087b361876b1e367db5463ca8a
Leobel Izquierdo <leobelizquierdo@gmail.com>
2023-02-18 16:46:03 -0300

    Create get_asset_metadata.sql

---
+++ get_asset_metadata.sql
@@ -0,0 +1,15 @@
+WITH "asset" AS (
+	SELECT 
+		MAX(tx_metadata.tx_id) AS LAST_MINTED_TX_ID
+-- 		tx_metadata.tx_id AS LAST_MINTED_TX_ID
+	FROM "ma_tx_mint"
+	INNER JOIN "multi_asset" AS "asset" ON "asset"."id" = "ma_tx_mint"."ident"
+	INNER JOIN "tx_metadata" ON "tx_metadata"."tx_id" = "ma_tx_mint"."tx_id"
+	WHERE ASSET.FINGERPRINT = 'asset1l6rg97vuuqf7ycqyz5lwkmvzu4s2hdqdlk0yk2'
+-- 	ORDER BY tx_metadata.tx_id DESC NULLS LAST LIMIT 1 -- (this is equivalent to MAX where null are ignored)
+-- 	;
+)
+SELECT 
+	JSONB_BUILD_OBJECT('json',TX_METADATA.JSON) || JSONB_BUILD_OBJECT('label',TX_METADATA.KEY) AS METADATA
+FROM "asset"
+INNER JOIN "tx_metadata" ON "tx_metadata"."tx_id" = "asset"."last_minted_tx_id";
